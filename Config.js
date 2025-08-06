6f05a012ba7b1f98d1544f3fc7fe473b136d9bc0
haenxhc <louiselimene310@gmail.com>
2025-08-06 12:23:32 -0400

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,9 @@
+js
+module.exports = {
+  owner: ['509XXXXXXXX'], // remplace par ton numéro complet avec l'indicatif
+  prefix: '!',
+  botName: 'Cheems-Bot',
+  footer: '© HAENXHC 2025',
+  sessionName: 'auth_info',
+  autoread: true
+}
